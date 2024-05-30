7acdfe181d19536e8951f773ff7e7175a995a878
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-05-30 21:22:11 +0200

    Projekt angelegt https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.3.0&packaging=jar&jvmVersion=17&groupId=de.eldecker.dhbw.spring&artifactId=blog&name=Blog&description=Einfaches%20CMS%20f%C3%BCr%20einen%20Blog&packageName=de.eldecker.dhbw.spring.blog&dependencies=thymeleaf,h2,web,data-jpa

---
+++ src/main/java/de/eldecker/dhbw/spring/blog/BlogApplication.java
@@ -0,0 +1,18 @@
+package de.eldecker.dhbw.spring.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Einstiegsklasse für Spring Boot.
+ */
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+        
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
---
+++ src/main/java/de/eldecker/dhbw/spring/blog/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Dieses Paket enthält eine mit <i>Spring Boot</i> entwickelte Web-Anwendung,
+ * die ein einfaches <i>Content Management System (CMS)</i> für einen Blog
+ * realisiert.
+ */
+package de.eldecker.dhbw.spring.blog;
