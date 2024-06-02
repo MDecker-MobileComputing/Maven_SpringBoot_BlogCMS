5c6480ca6ee80806eb40dcddeeb2fc0a1c4de7f6
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-06-02 10:11:16 +0200

    Bash-Skripte: chmod u+x

--- src/main/java/de/eldecker/dhbw/spring/blog/sicherheit/MeinUserDetailsService.java
+++ src/main/java/de/eldecker/dhbw/spring/blog/sicherheit/MeinUserDetailsService.java
@@ -14,7 +14,7 @@ import de.eldecker.dhbw.spring.blog.db.AutorenRepo;
 
 
 /**
- * Implementierung Interface {@code UserDetailsService}, von der bei Anmeldevorgang 
+ * Implementierung Interface {@code UserDetailsService}, von der beim Anmeldevorgang
  * eines Autors ein Nutzerobjekt für einen bestimmten Nutzernamen abgefragt.
  */
 @Service
@@ -24,48 +24,48 @@ public class MeinUserDetailsService implements UserDetailsService {
 
     /** Repo-Bean für Zugriff auf Datenbanktabelle mit Autoren. */
     private AutorenRepo _autorRepo;
-    
-    
+
+
     /**
      * Konstruktor für <i>Dependency Injection</i>.
      */
     @Autowired
     public MeinUserDetailsService( AutorenRepo autorRepo ) {
-        
+
         _autorRepo = autorRepo;
     }
-    
-    
+
+
     /**
      * Diese Methode wird aufgerufen, wenn ein Nutzer {@code nutzername} und ein Passwort
      * im Anmeldeformular eingegeben hat. Es wird dann in der Datenbank nachgeschaut, ob es
-     * einen aktiven Nutzer mit {@code nutzername} gibt. 
+     * einen aktiven Nutzer mit {@code nutzername} gibt.
      *
      * @param nutzername Im Anmeldeformular eingegebener Nutzername des Autors
      *
-     * @return Nutzer-Objekt für {@code nutzername}, enthält u.a. Passwort.
+     * @return Nutzer-Objekt für {@code nutzername}, enthält u.a. Passwort im Bcrypt-Format
      *
      * @throws UsernameNotFoundException Es gibt keinen Nutzer mit {@code nutzername}
-     */    
+     */
     @Override
     public UserDetails loadUserByUsername( String nutzername ) throws UsernameNotFoundException {
 
         final Optional<AutorEntity> autorOptional = _autorRepo.findByName( nutzername );
-        
+
         if ( autorOptional.isEmpty() ) {
-            
-            throw new UsernameNotFoundException( 
-                            "Kein Autor mit Name \"" + nutzername + "\" gefunden" );            
+
+            throw new UsernameNotFoundException(
+                            "Kein Autor mit Name \"" + nutzername + "\" gefunden" );
         } else {
-            
+
             final AutorEntity autorEntity = autorOptional.get();
-            
+
             final String passwort = autorEntity.getPasswort();
-            
+
             final UserDetails userDetails = User.withUsername( nutzername )
                                                 .password( "{bcrypt}" + passwort )
                                                 .roles( ROLLE_AUTOR )
-                                                .build();            
+                                                .build();
             return userDetails;
         }
     }
