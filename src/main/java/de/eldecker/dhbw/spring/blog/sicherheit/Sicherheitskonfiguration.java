035235a54a24feec3e44fe340e97274aff358ba2
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-06-10 13:39:29 +0200

    Update Sicherheitskonfiguration.java

--- src/main/java/de/eldecker/dhbw/spring/blog/sicherheit/Sicherheitskonfiguration.java
+++ src/main/java/de/eldecker/dhbw/spring/blog/sicherheit/Sicherheitskonfiguration.java
@@ -1,12 +1,10 @@
 package de.eldecker.dhbw.spring.blog.sicherheit;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
 
 
 /**
